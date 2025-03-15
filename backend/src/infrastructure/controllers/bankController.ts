8c703740d14680f93a01de07f6ad8e8cad2d13ee
Yanis Amarouayache <yanis.amarouayache@thalesgroup.com>
2025-03-15 16:01:00 +0100

    refactor : controller

--- backend/src/infrastructure/controllers/bankController.ts
+++ backend/src/infrastructure/controllers/bankController.ts
@@ -8,7 +8,7 @@ export const getBalance = (req: Request, res: Response) => {
 
 export const depositMoney = (req: Request, res: Response) => {
     const { amount } = req.body;
-    if (typeof amount !== 'number' || amount <= 0) {
+    if (!isValidAmount(amount)) {
         res.status(400).json({ message: "Invalid deposit amount" });
     }
     try {
@@ -16,15 +16,13 @@ export const depositMoney = (req: Request, res: Response) => {
         res.json({ message: "Deposit successful", newBalance: bankService.getBalance() });
     }
     catch (error) {
-        const err = error as Error;
-        res.status(500).json({ message: "An error occurred during deposit", error: err.message });
-        console.error("Deposit error:", error);
+       handleError(res, error, "An error occurred during deposit");
     }
 }
 
 export const withdrawMoney = (req: Request, res: Response) => {
     const { amount } = req.body;
-    if (typeof amount !== 'number' || amount <= 0) {
+    if (!isValidAmount(amount)) {
         res.status(400).json({ message: "Invalid withdrawal amount" });
     }
     if (bankService.getBalance() < amount) {
@@ -35,8 +33,16 @@ export const withdrawMoney = (req: Request, res: Response) => {
         res.json({ message: "Withdrawal successful", newBalance: bankService.getBalance() });
     }
     catch (error) {
-        const err = error as Error;
-        res.status(500).json({ message: "An error occurred during withdrawal", error: err.message });
-        console.error("Withdrawal error:", error);
+        handleError(res, error, "An error occurred during withdrawal");
     }
+}
+
+function isValidAmount(amount: any): boolean {
+    return typeof amount === 'number' && amount > 0;
+}
+
+function handleError(res: Response, error: any, message: string) {
+    const err = error as Error;
+    res.status(500).json({ message, error: err.message });
+    console.error(message, error);
 }
